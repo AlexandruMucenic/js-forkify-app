7cc0e9c1fe974a687697e1e41b539feaa2af170a
“AlexandruMucenic” <alexmucenic6@gmail.com>
2022-11-20 18:47:44 +0200

    changed api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const API_KEY = '8c04b754-c087-4a30-9950-b4b900c45935';
+export const API_KEY = '914ab4e5-ac17-46ff-ab95-dd43c8f0ee63';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const MODAL_CLOSE_SEC = 2.5;
